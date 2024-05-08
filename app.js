ff3931c27d0803949fdc2662f22bfc16b398fbe4
Calfura <travis_wilson999@hotmail.com>
2024-05-08 13:52:07 +1000

    feat: class exmaples

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// Impoprt library class, Book class, Audio book class, Games class, Movies class, Music class
+
+const { Media } = require("./Meida");
+
+// Create instances of classes 
+
+let lotr = new Media("Fellowship of the Ring");
+
+console.log(lotr);
+
+// Lord of the rings as a instance of book class
+
+// Do things with those isntaces
+
+// Create an instance of a library
+// add media instances into tthe Library instance
+
+// Log the data in the Library
